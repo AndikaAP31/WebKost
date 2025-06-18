01d4b7ae044239c46912fe146e2a3ad3273710e9
andikaarsana <andikaarsana31@gmail.com>
2025-06-18 21:26:41 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.{blade.php,js}",
+    "./resources/views/**/*.blade.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
